18a2dbd1244c21f47664a9530ce363909f4e50f8
Willem <willem.browne@gmail.com>
2015-07-21 14:00:23 -0400

    fixed bootstrap-select

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require bootstrap-select
 //= require zeroclipboard
 //= require d3
 //= require turbolinks
